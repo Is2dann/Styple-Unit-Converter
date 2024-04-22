78cf50c9d7ad30671e55f94bb8806187edd37149
Is2dann <144619269+Is2dann@users.noreply.github.com>
2024-04-22 13:39:49 +0100

    add js code for marquee text

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// First of all, the automation for the moving text
+i = 0;
+  // text to be showed on the main page
+myText = 'Simple yet stylish unit converter!';
+  // as text is not too long I added 250 milliseconds, so it's not too fast and readable
+let sequence1 = setInterval(update, 250);
+function update() {
+  i++;
+  if (i > myText.length) {
+    // i is set to zero so it's start over when the text rolled out
+    i = 0;
+  }
+  moveLeft1 = myText.substring(i, myText.length);
+    // non-breaking spaces added to the end
+  moveLeft1 += '&nbsp;';
+    // start at the beginning and up to i to create the marquee effect
+  moveLeft1 += myText.substring(0, i);
+    // finally apply the text into html page
+  document.getElementById('move-left1').innerHTML = moveLeft1;
+}
